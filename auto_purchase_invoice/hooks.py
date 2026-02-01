ac761467eecc7e6ea4446fd44ee8a00090d47fba
garystarr-surgi <gary.starr@surgishop.com>
2026-02-01 15:18:34 -0500

    Update hooks.py

--- auto_purchase_invoice/hooks.py
+++ auto_purchase_invoice/hooks.py
@@ -4,3 +4,11 @@ app_publisher = "Gary Starr"
 app_description = "Auto creates Purchase Invoice when Purchase Receipt reaches Putaway in Progress"
 app_email = "gary.starr@surgishop.com"
 app_license = "MIT"
+
+# ... existing content ...
+
+doc_events = {
+    "Purchase Receipt": {
+        "on_submit": "auto_purchase_invoice.auto_purchase_invoice.on_purchase_receipt_submit"
+    }
+}
