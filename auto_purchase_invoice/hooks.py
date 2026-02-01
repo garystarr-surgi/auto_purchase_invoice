a21aee7ed86301cb6bb8a7593f040e5326be2a47
garystarr-surgi <gary.starr@surgishop.com>
2026-02-01 12:45:20 -0500

    Update hooks.py

--- auto_purchase_invoice/hooks.py
+++ auto_purchase_invoice/hooks.py
@@ -1,1 +1,6 @@
-
+app_name = "auto_purchase_invoie"
+app_title = "Auto Purchase Invoice"
+app_publisher = "SurgiShop"
+app_description = "Auto creates Purchase Invoice
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
