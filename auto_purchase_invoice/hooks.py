fdbdaa76dd2f1d6b1e869ddc1c7412dd53230b99
garystarr-surgi <gary.starr@surgishop.com>
2026-02-01 12:57:40 -0500

    Update hooks.py

--- auto_purchase_invoice/hooks.py
+++ auto_purchase_invoice/hooks.py
@@ -3,4 +3,6 @@ app_title = "Auto Purchase Invoice"
 app_publisher = "SurgiShop"
 app_description = "Auto creates Purchase Invoice
 app_email = "gary.starr@surgishop.com"
-app_license = "MIT"
+app_license = "MIT
+required_apps = ["frappe/erpnext"]
+required_frappe_version = ">=16.0.0"
